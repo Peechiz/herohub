e8983ae57df689f53779550ce99d1741ad929d46
Chris Impicciche <cimpicci@gmail.com>
2018-02-13 12:11:33 -0600

    moves heroprogress to own component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import heroes from '../mock/topHeroes.json'
-import playtime from '../mock/playtime.json'
+import TopHeroes from './HeroProgress.jsx'
 import '../css/topHeroes.css'
 import '../css/profile.css'
 
@@ -24,9 +23,9 @@ const PlayerInfo = (props) => {
   }
   return (
     <div className="col-md-6">
-      <div class="row">
-        <div class="lvl" style={bk}>
-          <div class="num">6</div>
+      <div className="row">
+        <div className="lvl" style={bk}>
+          <div className="num">6</div>
         </div>
         <h1>Player Name</h1>
       </div>
@@ -35,59 +34,5 @@ const PlayerInfo = (props) => {
   )
 }
 
-
-class TopHeroes extends Component {
-  constructor() {
-    super();
-    this.state = {
-      heroes: {},
-      playtime: []
-    }
-  }
-  componentWillMount(){
-
-    // get sorted array of playtime from playtime object
-    let sorted = Object.keys(playtime).reduce((arr, key) => {
-      arr.push({
-        name: key,
-        time: playtime[key]
-      })
-      return arr
-    }, []).sort((a,b) => b.time - a.time )
-
-    this.setState({
-      heroes,
-      playtime: sorted
-    })
-  }
-  render(){
-    let playtime = this.state.playtime
-    let info = this.state.heroes
-    return (
-      <div className="col-md-6">
-        <h3>Top Heroes</h3>
-        <div className="hero-progress">
-          { playtime.map(hero => <HeroProgress key={hero.name} hero={hero} info={info}/>) }
-        </div>
-      </div>
-    )
-  }
-}
-
-const HeroProgress = (props) => {
-  let {hero, info} = props;
-  return (
-    <div className="progress-item">
-      <img src={info[hero.name].thumb} alt={`${hero.name}`}/>
-      <div className="bar-container">
-        <div className="bar" style={{ backgroundColor:info[hero.name].color, boxShadow:info[hero.name].shadow}}></div>
-        <div className="bar-text">
-          <div className="title">{hero.name}</div>
-          <div className="description">{Math.round(hero.time)} hours</div>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 export default Profile
