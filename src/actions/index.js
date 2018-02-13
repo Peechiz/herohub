f003f9ebbaf9b98e9582f9643f838462a4c7aa68
Chris Impicciche <cimpicci@gmail.com>
2018-02-13 16:09:51 -0600

    adds updatePlayer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
 export const toggleMode = () => {
+  // console.log('action: toggleMode')
   return {
     type: 'TOGGLE_MODE',
+  }
+}
+
+export const updatePlayer = (player, battletag) => {
+  return {
+    type: 'CHANGE_PLAYER',
+    player,
+    battletag
   }
 }
