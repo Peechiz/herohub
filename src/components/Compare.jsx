bbe40aff78cf03eb930229636263fca57b9cb7cd
Chris Impicciche <cimpicci@gmail.com>
2018-02-15 15:20:44 -0600

    adds hero category or hero selection from data

--- src/components/Compare.jsx
+++ src/components/Compare.jsx
@@ -1,36 +1,64 @@
 import React, {Component} from 'react'
 import { HeroProgress } from './HeroProgress.jsx'
 import { connect } from 'react-redux'
+import { selectRole, heroSelect } from '../actions'
 
-import info from '../mock/heroInfo.json'
+// import info from '../mock/heroInfo.json'
 import '../css/compare.css'
-
-let dva = {
-  name: 'dva',
-  time: 12.468
-}
+import { getInfoArray } from '../util'
 
 class Compare extends Component {
 
+  constructor(props) {
+    super(props);
+    [
+      'roleSelect',
+      'heroSelect'
+    ].forEach(fn => this[fn] = this[fn].bind(this))
+  }
+
+  roleSelect(e) {
+    this.props.dispatch(selectRole(e.target.value))
+    this.props.dispatch(heroSelect('top'))
+  }
+
+  heroSelect(e) {
+    this.props.dispatch(heroSelect(e.target.value))
+  }
+
   render() {
-    // console.log(this.props)
-    const p1 = this.props.players.p1
-    const p2 = this.props.players.p2
+    const {
+      p1, p2, role, mode, selectedHero, heroInfo, filteredHeroes
+    } = this.props
+
+    if (selectedHero === 'top') {
+      p1.hero = getTopHero(role, mode, p1, heroInfo);
+      p2.hero = getTopHero(role, mode, p2, heroInfo);
+    } else if (selectedHero) {
+      console.log(selectedHero)
+      p1.hero = getHeroPlaytime(selectedHero, mode, p1);
+      p2.hero = getHeroPlaytime(selectedHero, mode, p2);
+    }
+
     return (<div>
       <div className="row">
         <div className="col-md-5">
-          <img src="/img/dva.png" height="200" alt=""/>
+          {p1.hero ?
+            <img src={`/img/${p1.hero.key}.png`} height="200" style={{transform: 'scaleX(-1)'}}alt=""/> : null
+          }
         </div>
         <div className="col-md-2 flex-mid">
-            <select className="form-control">
-              <option value="">Tank</option>
-              <option value="">Support</option>
-              <option value="">Offense</option>
-              <option value="">Defense</option>
-            </select>
+          <RoleSelect handleChange={this.roleSelect}/>
+          { this.props.role ?
+            <HeroSelect handleChange={this.heroSelect} info={filteredHeroes}/>
+            : ''
+          }
         </div>
         <div className="col-md-5">
-          <img src="/img/dva.png" height="200" style={{transform: 'scaleX(-1)'}}alt=""/>
+          {console.log(p2.hero)}
+          {p2.hero ?
+            <img src={`/img/${p2.hero.key}.png`} height="200" style={{transform: 'scaleX(-1)'}}alt=""/> : null
+          }
         </div>
       </div>
 
@@ -40,7 +68,9 @@ class Compare extends Component {
           <div style={{"padding": '0 1em'}}>
             <h1>{p1.battletag}</h1>
           <div>
-              <HeroProgress invert="true" hero={dva} info={info[dva.name]}/>
+              {p1.hero ?
+                <HeroProgress invert="true" hero={p1.hero} info={heroInfo[p1.hero.key]}/> : null
+              }
             </div>
           </div>
         </div>
@@ -55,7 +85,9 @@ class Compare extends Component {
           <div style={{padding: '0 1em'}}>
             <h1>{p2.battletag}</h1>
             <div>
-              <HeroProgress hero={dva} info={info[dva.name]}/>
+              {p2.hero ?
+                <HeroProgress hero={p2.hero} info={heroInfo[p2.hero.key]}/> : null
+              }
             </div>
           </div>
         </div>
@@ -68,11 +100,71 @@ class Compare extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    mode: state.mode,
-    players: state.players
+const filterHeroesByRole = (heroes,role) => {
+  switch (role) {
+    case 'tank':
+      return heroes.filter(hero => hero.role === 'tank')
+    case 'offense':
+      return heroes.filter(hero => hero.role === 'offense')
+    case 'defense':
+      return heroes.filter(hero => hero.role === 'defense')
+    case 'support':
+      return heroes.filter(hero => hero.role === 'support')
+    default:
+      return heroes
   }
+}
+
+function getTopHero (role,mode,player, heroInfo) {
+  const heroInfoArr = getInfoArray(heroInfo)
+  let heroesInRole = heroInfoArr.filter(h => h.role === role)
+  const playtime = player.us.heroes.playtime[mode]
+
+  const top = heroesInRole.map(hero => {
+    let h = {}
+    h.time = playtime[hero.key]
+    h.key = hero.key
+    return h
+  }).sort((a,b) => b.time - a.time )[0]
+
+  return top
+}
+
+function getHeroPlaytime(hero, mode, player, heroInfo){
+  return {
+    key: hero,
+    time: player.us.heroes.playtime[mode][hero],
+  }
+}
+
+const mapStateToProps = (state) => {
+  const heroes = getInfoArray(state.heroInfo)
+
+  return {
+    ...state,
+    filteredHeroes: filterHeroesByRole(heroes,state.role)
+  }
+}
+
+const RoleSelect = (props) => {
+  return (
+    <select defaultValue='' onChange={props.handleChange} className="form-control">
+      <option disabled></option>
+      <option value="offense">Offense</option>
+      <option value="defense">Defense</option>
+      <option value="tank">Tank</option>
+      <option value="support">Support</option>
+    </select>
+  )
+}
+
+const HeroSelect = ({handleChange, info}) => {
+  return (
+    <select defaultValue='' className="form-control" onChange={handleChange}>
+      <option value='top'>Top Played</option>
+      { info.map(hero => <option key={hero.key} value={hero.key}>{hero.name}</option>) }
+    </select>
+  )
 }
 
 const Stat = ({title, stat1, stat2}) => {
