38845d046229533ce822ef86e0811b6de9955359
Chris Impicciche <cimpicci@gmail.com>
2018-02-15 15:17:03 -0600

    refactors player reducers, adds role, heroInfo, and selectedHero to app state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,36 +1,38 @@
 import { combineReducers } from 'redux'
 import data from '../mock/blob.json'
+import data2 from '../mock/blob2.json'
+import info from '../mock/heroInfo.json'
 
 const initialState = {
   mode: 'quickplay',
-  players: {
-    p1: {
-      data: data.us
-    },
-    p2: {}
+  p1: {
+    data: data.us
+  },
+  p2: {},
+  role: null
+}
+
+function createPlayer(playerName = '', data) {
+  return function playerReducer(state = data, action) {
+    const { player } = action;
+    if(player !== playerName) return state;
+
+    switch (action.type) {
+      case 'CHANGE_PLAYER':
+        return {
+          ...state,
+          battletag: action.battletag
+        }
+      default:
+        return state
+    }
   }
 }
 
-const players = (state = initialState.players, action) => {
+const role = (state = initialState.role, action) => {
   switch (action.type) {
-    case 'CHANGE_PLAYER':
-      if (action.player === 'p1'){
-        return {
-          ...state,
-          p1: {
-            ...state.p1,
-            battletag: action.battletag
-          }
-        }
-      } else {
-        return {
-          ...state,
-          p2: {
-            ...state.p2,
-            battletag: action.battletag
-          }
-        }
-      }
+    case 'SELECT_ROLE':
+      return action.role
     default:
       return state
   }
@@ -46,9 +48,22 @@ const mode = (state = initialState.mode, action) => {
   }
 }
 
+const selectedHero = (state = '', action) => {
+  return action.type === 'SELECT_HERO' ?
+    action.hero : state
+}
+
+const heroInfo = (state = info, action) => {
+  return state
+}
+
 const heroHub = combineReducers({
   mode,
-  players
+  p1: createPlayer('p1', data),
+  p2: createPlayer('p2', data2),
+  role,
+  heroInfo,
+  selectedHero
 })
 
 
